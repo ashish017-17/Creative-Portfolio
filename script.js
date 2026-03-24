fee525ccd924d07571954d0f380977eff23e1d2d
ashish017-17 <ashish03724@gmail.com>
2026-03-25 00:25:52 +0530

    loding animation

---
+++ script.js
@@ -0,0 +1,20 @@
+let tl = gsap.timeline();
+
+tl
+    .to("#fs", {
+        height:0,
+        duration: 2,
+        ease: Expo.easeInOut
+    })
+    .to("#elem",{
+        height: "100%",
+        duration: 2,
+        delay: -2,
+        ease: Expo.easeInOut
+    })
+    .to("#white",{
+        height: "100%",
+        duration: 2,
+        delay: -1.6,
+        ease: Expo.easeInOut
+    })
